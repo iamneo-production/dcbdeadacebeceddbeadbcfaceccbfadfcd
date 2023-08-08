d4ff81c59ddd348ce2282e528a3d134d45e5ed3f
Iamneo <techadmin@iamneo.ai>
2023-08-08 05:57:20 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,31 +1,22 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'pipes';
-  fromCurr :String = '';
-  toCurr   :string = '';
-  amount !:number;
-  result: string = 'sample';
-  show:boolean = false;
-
-
-  showResult()
-  {
-    console.log(this.fromCurr);
-    console.log(this.toCurr);
-    console.log(this.amount);
-    if(this.fromCurr !='' && this.toCurr != '')
-    {
-      this.show = true;
-    }
-    else
-    {
-      this.show = false;
-    }
-  }
+import { Component } from '@angular/core'; 
+//import { FormsModule } from '@angular/forms'; 
+@Component({ 
+  selector: 'app-root', 
+  templateUrl: './app.component.html', 
+  styleUrls: ['./app.component.css'] 
+}) 
+export class AppComponent { 
+  title = 'angularapp'; 
+  value1: string = '';  
+  value2: string = '';  
+  result: string ='';  
+  r1:string=""; 
+  amount1: number = 0;  
+  today=new Date();  
+  handle(sortoption1: any,sortoption2: any,amount1: any){ 
+    console.log(sortoption1,sortoption2,amount1); 
+    this.result=(Math.round((sortoption2/sortoption1)*amount1)).toFixed(2) 
+    this.r1=this.result; 
+      
+  } 
 }
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,24 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CurrencyconvPipe } from './currencyconv.pipe';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    CurrencyconvPipe
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+import { NgModule } from '@angular/core'; 
+import { BrowserModule } from '@angular/platform-browser'; 
+import { FormsModule } from '@angular/forms'; 
+import { AppComponent } from './app.component'; 
+//import { FormComponent } from './form/form.component'; 
+import { AppRoutingModule } from './app-routing.module'; 
+ 
+@NgModule({ 
+  declarations: [ 
+    AppComponent 
+    //FormComponent 
+  ], 
+  imports: [ 
+    BrowserModule, 
+    AppRoutingModule, 
+    FormsModule 
+  ], 
+  providers: [], 
+  bootstrap: [AppComponent] 
+}) 
 export class AppModule { }
